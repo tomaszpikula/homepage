67ae7c3b9219e48809c2d2eb430259d54c4cd835
Tomasz Pikuła <tomasz.pikula.2@gmail.com>
2021-02-11 22:11:24 +0100

    BEM, styles, media queries

--- main.js
+++ main.js
@@ -2,3 +2,36 @@ const firstName = 'Antoni';
 const age = '51';
 
 console.log(`Nazywam się ${firstName} i mam ${age} lat `);
+
+const emptyElement = document.querySelector('.introduction__description--js');
+
+emptyElement.innerHTML = 'Tekst wprowadzony przy pomocy JS';
+
+function welcome (firstName, age) {
+    console.log(`Witaj ${firstName}, dobrze mieć ${age} lat`);
+}
+
+welcome(firstName, age);
+
+const greet = (firstName, age) => {
+    console.log(`Witaj ${firstName}, dobrze mieć ${age} lat (fat arrow)`);
+}
+
+greet(firstName, age);
+
+greet('Jan', 30);
+
+function calculate (myNumber){
+   myNumber = myNumber + 33;
+    console.log(myNumber);
+    return myNumber*7;
+}
+
+const myResult = calculate(5);
+
+const hamburger = document.querySelector('.hamburger--js');
+
+hamburger.addEventListener('click', () => {
+    const nav = document.querySelector('.navigation--js');
+    nav.classList.toggle('navigation--open');
+})
